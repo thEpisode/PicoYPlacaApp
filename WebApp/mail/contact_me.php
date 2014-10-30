77f452c6635417513b4d445c603009a2e47d7e2d
Cami Rodriguez <camiepisode@outlook.com>
2014-10-30 16:46:27 -0500

    Try to correct a contact form

--- WebApp/mail/contact_me.php
+++ WebApp/mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'camilo.rodriguezc@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Test from Pico y Placa Web App:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: camiepisode@outlook.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
